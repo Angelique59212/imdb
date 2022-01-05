57f03640cae834a0a9ec98de91f8cb7dd54f7316
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-05 15:45:12 +0100

    js create array & create footer

---
+++ js/arrMovies.js
@@ -0,0 +1,37 @@
+let arrMovies = [
+    [
+        'imgMovies/batman.jpg',
+        'imgMovies/batman02.jpg',
+        'Robert Pattinson and Zoë Kravitz Team Up in \'The Batman\''
+    ],
+    [
+        'imgMovies/death.jpg',
+        'imgMovies/death02.jpg',
+        'Gal Gadot in \'Death on the Nile\''
+    ],
+    [
+        'imgMovies/doctor.jpg',
+        'imgMovies/doctor02.jpg',
+        'Doctor Strange in the Multiverse of Madness'
+    ],
+    [
+        'imgMovies/matrix.jpg',
+        'imgMovies/matrix02.jpg',
+        'Keanu Reeves Says: See \'The Matrix Resurrections\' 3 Times'
+    ],
+    [
+        'imgMovies/peaky.jpg',
+        'imgMovies/peaky02.jpg',
+        '"Peaky Blinders" Season 6'
+    ],
+    [
+        'imgMovies/uncharted02.jpg',
+        'imgMovies/uncharted.jpg',
+        'Mark Walhberg & Tom Holland Go Treasure Hunting'
+    ],
+    [
+        'imgMovies/witcher.jpg',
+        'imgMovies/witcher02.jpg',
+        '"The Witcher" Stars Debate the Scariest Monster in Season 2'
+    ]
+]
