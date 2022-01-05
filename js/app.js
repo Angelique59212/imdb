c4a5601134b0e43a85ca4e28d0ce2158cfe65ceb
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-05 14:03:30 +0100

    caroussel

--- js/app.js
+++ js/app.js
@@ -7,3 +7,17 @@ $(document).ready(function(e){
         $('.input-group #search_param').val(param);
     });
 });
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
